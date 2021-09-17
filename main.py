6d6fdc01a01ca2d83a68bd36ae356213ef6e8922
abhishekra07 <abhira719@gmail.com>
2021-09-17 14:01:02 +0530

    added drag for rectangle

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 
-
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 detector = HandDetector(0.8)
 colorR = (255, 0, 255)
+cx, cy, w, h = 100, 100, 200, 200
 
 while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
-    img = img = detector.findHands(img)
+    img = detector.findHands(img)
     landmarkList, _ = detector.findPosition(img)
     if landmarkList:
-        cursor = landmarkList[8]
-        if 100 < cursor[0] < 300 and 100 < cursor[1] < 300 :
-            colorR = (0, 255, 0)
 
+        l,_, _ = detector.findDistance(8,12,img)
+        print(l)
+        if l < 30:
+            cursor = landmarkList[8]
+            if cx-w//2 < cursor[0] < cx+w//2 and cy-h//2 < cursor[1] < cy+h//2:
+                colorR = (0, 255, 0)
+                cx, cy = cursor
+        else :
+            colorR = (255, 0, 255)
 
-    cv2.rectangle(img, (100,100), (300,300), colorR, cv2.FILLED)
+    cv2.rectangle(img, (cx-w//2, cy-h//2), (cx+w//2, cy+h//2), colorR, cv2.FILLED)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
