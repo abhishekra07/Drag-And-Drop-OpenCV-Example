e241e4976303e7895fe60052cae91f76ca9e085f
abhishekra07 <abhira719@gmail.com>
2021-09-17 13:47:49 +0530

    changing rectangle color on hover

--- main.py
+++ main.py
@@ -6,10 +6,20 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 detector = HandDetector(0.8)
+colorR = (255, 0, 255)
 
 while True:
     success, img = cap.read()
+    img = cv2.flip(img, 1)
     img = img = detector.findHands(img)
-    landmarkList, _ = detector.findPosition(img);
+    landmarkList, _ = detector.findPosition(img)
+    if landmarkList:
+        cursor = landmarkList[8]
+        if 100 < cursor[0] < 300 and 100 < cursor[1] < 300 :
+            colorR = (0, 255, 0)
+
+
+    cv2.rectangle(img, (100,100), (300,300), colorR, cv2.FILLED)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
